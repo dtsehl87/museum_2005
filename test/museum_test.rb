c2668f9156d7d280f06d5e58494974556818b398
Dan Sehl <dan.sehl@gmail.com>
2020-05-27 09:18:02 -0600

    Write passing name test for museum

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,4 +5,15 @@ require './lib/exhibit'
 require './lib/museum'
 
 class MuseumTest < Minitest::Test
+
+  def test_it_exists
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    assert_instance_of Museum, dmns
+  end
+
+  def test_it_has_a_name
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+  end
+
 end
