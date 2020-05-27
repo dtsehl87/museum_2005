21cf82081852cf414f847012c51076d2117cc237
Dan Sehl <dan.sehl@gmail.com>
2020-05-27 09:13:07 -0600

    Write passing default interests test for patron

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -20,4 +20,9 @@ class PatronTest < Minitest::Test
     assert_equal 20, patron_1.spending_money
   end
 
+  def test_it_defaults_to_no_interests
+    patron_1 = Patron.new("Bob", 20)
+    assert_equal [], patron_1.interests
+  end
+
 end
